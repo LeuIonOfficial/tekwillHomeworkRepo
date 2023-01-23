cd611bdc2109cabf05d14169ab116111ee2cf8c5
Ion Leu <leuionofficial@gmail.com>
2023-01-23 22:33:53 +0200

    commited lesson 19 homework exercise 1 (Human.py)

--- lesson_18_homework/ex_3.py
+++ lesson_18_homework/ex_3.py
@@ -6,7 +6,7 @@ def benchmark_time(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"It took {end_time - start_time:.2f} seconds to execute {func.__name__}".format())
+        print(f"It took {end_time - start_time:.2f} seconds to execute {func.__name__}")
         return result
 
     return wrapper
---
+++ lesson_19_homework/Human.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+DATE_FORMAT = '%Y/%m/%d'
+
+
+class Human:
+
+    def __init__(self, first_name: str, last_name: str, dob):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+
+    def __str__(self):
+        pass
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_human_age(self):
+        today = datetime.now()
+        dob = datetime.strptime(self.dob, DATE_FORMAT)
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        return age
+
+
+me = Human("Ion", "Leu", "2001/05/09")
+print(f"{me.get_full_name()}, age {me.get_human_age()}")
